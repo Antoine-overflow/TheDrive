7accb45c55cd09ab06d15b958e794d538d6b1677
mahanonu <hermantheo@hotmail.fr>
2022-02-15 14:07:56 +0100

    Creating launch shell

---
+++ glusterfs_config/launch.sh
@@ -0,0 +1,8 @@
+chmod 700 setup.sh
+chmod 700 config_glusterrest.sh
+chmod 700 launch_gluster_plugin.sh
+chmod 700 docker-volume-glusterfs-amd64
+
+./setup.sh
+./config_glusterrest.sh
+./launch_gluster_plugin.sh
--- glusterfs_config/setup.sh
+++ glusterfs_config/setup.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 
-sudo apt-get install python3.8
-sudo apt-get install git
-sudo apt-get install gunicorn
-sudo apt-get install golang-go 
+sudo apt-get install -y python3.8
+sudo apt-get install -y git
+sudo apt-get install -y gunicorn
+sudo apt-get install -y golang-go 
