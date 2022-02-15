6d33587a0f8a0c1140742c30790a22e9109c9db5
mahanonu <hermantheo@hotmail.fr>
2022-02-15 14:38:00 +0100

    creating gluster_rest service

--- glusterfs_config/config_glusterrest.sh
+++ glusterfs_config/config_glusterrest.sh
@@ -4,4 +4,7 @@ git checkout fixing_glusterrest_install
 sudo python3 setup.py install
 sudo glusterrest install
 
-sudo glusterrestd
+mv gluster_rest.service /etc/systemd/system/
+
+systemctl daemon-reload
+systemctl start gluster_rest && systemctl enable gluster_rest
