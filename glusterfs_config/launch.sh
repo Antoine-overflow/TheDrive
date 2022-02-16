cd078cb3879d6493f75ee25df386d120ed276119
mahanonu <hermantheo@hotmail.fr>
2022-02-16 12:08:08 +0100

    add mkdir

--- glusterfs_config/launch.sh
+++ glusterfs_config/launch.sh
@@ -2,7 +2,7 @@ chmod 700 setup.sh
 chmod 700 config_glusterrest.sh
 chmod 700 launch_gluster_plugin.sh
 chmod 700 docker-volume-glusterfs-amd64
-
+mkdir -p /var/lib/gluster/volumes
 ./setup.sh
 ./config_glusterrest.sh
 ./launch_gluster_plugin.sh
