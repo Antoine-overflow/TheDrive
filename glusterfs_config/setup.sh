ff25f0ce71019281c3fde43ef40dcca2009f0d44
mahanonu <hermantheo@hotmail.fr>
2022-02-16 09:57:39 +0100

    add all installs for api

--- glusterfs_config/setup.sh
+++ glusterfs_config/setup.sh
@@ -1,5 +1,7 @@
 sudo apt-get update
 
 sudo apt-get install -y python3.8
-sudo apt-get install -y git
 sudo apt-get install -y gunicorn
+sudo apt-get install -y argparse
+sudo apt-get install -y flask 
+sudo apt-get install -y pyyaml
