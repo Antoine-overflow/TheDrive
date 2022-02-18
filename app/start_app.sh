4fda91716efa27b9b1bbe8a0421b977247d97bf3
Axelle-Ga <axelle.gaige@gmail.com>
2022-02-18 15:09:54 +0000

    save

--- app/start_app.sh
+++ app/start_app.sh
@@ -5,8 +5,9 @@ openssl rand -base64 32 > .secrets/mysql_root_password
 docker stack deploy -c config.yml gnext
 docker service ls
 sleep 1
+docker service scale gnext_seed=1
 docker service scale gnext_node=2
 docker service scale gnext_seed=0
 docker service scale gnext_node=3
-docker service scale gnext_nextcloud=1
+docker service scale gnext_nextcloud=3
 docker service scale gnext_onlyoffice=1
